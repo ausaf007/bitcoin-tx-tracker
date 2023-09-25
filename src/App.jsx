4b5b3a077baa901fd5c90fc7bc18359bead89052
Md Ausaf Rashid <ausafrashid111@gmail.com>
2023-09-26 00:22:25 +0530

    Added selective disabling of buttons

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,15 @@ import Header from "./components/Header.jsx";
 import { startTransactionMonitoring, stopTransactionMonitoring } from './services/TxMonitorService';
 
 function App() {
+    // This is to update the status  component in real time
     const [status, setStatus] = useState('Not Monitoring');
 
+    // This is to selectively enable the monitoring buttons
+    const [isMonitoring, setIsMonitoring] = useState(false);
     const handleStartMonitoring = async (txId) => {
         try{
             await startTransactionMonitoring(txId, setStatus);
+            setIsMonitoring(true);
         }catch (error){
             console.error("Error starting transaction monitoring:", error);
         }
@@ -22,6 +26,7 @@ function App() {
         try{
             await stopTransactionMonitoring();
             setStatus('Not Monitoring');
+            setIsMonitoring(false);
         }catch (error){
             console.error("Error stopping transaction monitoring:", error);
         }
@@ -31,7 +36,11 @@ function App() {
         <div className="centered">
             <div className="App" >
                 <Header/>
-                <TxMonitor onStartMonitoring={handleStartMonitoring} onStopMonitoring={handleStopMonitoring}/>
+                <TxMonitor
+                    onStartMonitoring={handleStartMonitoring}
+                    onStopMonitoring={handleStopMonitoring}
+                    isMonitoring={isMonitoring}
+                />
                 <StatusDisplay status={status}/>
             </div>
         </div>
